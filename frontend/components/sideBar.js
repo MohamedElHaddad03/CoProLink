580d66d7a2d2801f142b344d77595286fc4e7467
MohamedElHaddad03 <130052591+MohamedElHaddad03@users.noreply.github.com>
2023-12-20 22:27:56 +0100

    sidebar update

--- frontend/components/sideBar.js
+++ frontend/components/sideBar.js
@@ -8,7 +8,8 @@ const Sidebar = ({ selectedItem, setSelectedItem }) => {
   const largeur = useRef(new Animated.Value(50)).current;
   const iconColor = isSidebarOpen ? '#3b67bb' : '#fff';
   const bgColor = isSidebarOpen ? '#fff' : '#3b67bb';
- 
+  const bgColorOpen = isSidebarOpen ? '#3b67bb20' : '#26467c';
+
   const toggleSidebar = () => {
     const newValue = isSidebarOpen ? 50 : 200;
     Animated.timing(largeur, {
@@ -41,41 +42,59 @@ const handleItemClick = (item) => {
           )}
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={[styles.sidebarItem]}  onPress={() => handleItemClick('Statistiques')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Statistiques' && {backgroundColor:bgColorOpen},
+  ]}  onPress={() => handleItemClick('Statistiques')}>
       <Ionicons name={ selectedItem === "Statistiques"? "stats-chart":"stats-chart-outline"} size={26} color={iconColor} />
         {isSidebarOpen && (
           <Text style={[styles.sidebarText, { color: iconColor }]}>Statistiques</Text>
         )}
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.sidebarItem} onPress={() => handleItemClick('Documents')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Documents' && {backgroundColor:bgColorOpen},
+  ]} onPress={() => handleItemClick('Documents')}>
       <Ionicons name={ selectedItem === "Documents"? "documents":"documents-outline"} size={26} color={iconColor} />
         {isSidebarOpen && (
           <Text style={[styles.sidebarText, { color: iconColor }]}>Documents</Text>
         )}
       </TouchableOpacity>
-      <TouchableOpacity style={styles.sidebarItem} onPress={() => handleItemClick('Depenses')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Depenses' && {backgroundColor:bgColorOpen},
+  ]} onPress={() => handleItemClick('Depenses')}>
       <Ionicons name={ selectedItem === "Depenses"? "cash":"cash-outline"} size={26} color={iconColor} />
 
         {isSidebarOpen && (
           <Text style={[styles.sidebarText, { color: iconColor }]}>Dépenses</Text>
         )}
       </TouchableOpacity>
-      <TouchableOpacity style={styles.sidebarItem} onPress={() => handleItemClick('Paiement')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Paiement' && {backgroundColor:bgColorOpen},
+  ]} onPress={() => handleItemClick('Paiement')}>
       <Ionicons name={ selectedItem === "Paiement"? "card":"card-outline"} size={26} color={iconColor} />
 
         {isSidebarOpen && (
           <Text style={[styles.sidebarText, { color: iconColor }]}>Paiments</Text>
         )}
       </TouchableOpacity>
-      <TouchableOpacity style={styles.sidebarItem} onPress={() => handleItemClick('Profile')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Profile' && {backgroundColor:bgColorOpen},
+  ]} onPress={() => handleItemClick('Profile')}>
       <Ionicons name={ selectedItem === "Profile"? "person":"person-outline"} size={26} color={iconColor} />
 
         {isSidebarOpen && (
           <Text style={[styles.sidebarText, { color: iconColor }]}>Profil</Text>
         )}
       </TouchableOpacity>
-      <TouchableOpacity style={styles.sidebarItem} onPress={() => handleItemClick('Settings')}>
+      <TouchableOpacity style={[
+    styles.sidebarItem,
+    selectedItem === 'Settings' && {backgroundColor:bgColorOpen},
+  ]} onPress={() => handleItemClick('Settings')}>
       <Ionicons name={ selectedItem === "Settings"? "settings":"settings-outline"} size={26} color={iconColor} />
 
         {isSidebarOpen && (
@@ -90,7 +109,7 @@ const handleItemClick = (item) => {
 const styles = StyleSheet.create({
   sidebar: {
     zIndex:1,
-    padding: 10,
+  //  padding: 10,
     position: 'absolute', // Use absolute positioning for overlay
    // top: 0,
    paddingTop:StatusBar.currentHeight +10 ,
@@ -108,11 +127,14 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
   },
   sidebarItem: {
+    width: '100%',
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 40,
-    paddingRight: 5,
+    paddingHorizontal: 5,
   },
+  
   sidebarText: {
     color: '#F0F8FF',
     marginLeft: 10,
@@ -120,6 +142,7 @@ const styles = StyleSheet.create({
   sideBarbtn: {
     
   },
+  
 });
 
 export default Sidebar;
