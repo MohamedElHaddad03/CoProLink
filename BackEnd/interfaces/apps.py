0947ee865030ab996f35a06de47ae0c54f011047
MohamedElHaddad03 <130052591+MohamedElHaddad03@users.noreply.github.com>
2023-12-22 20:01:22 +0100

    delete

--- BackEnd/interfaces/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class InterfacesConfig(AppConfig):
-    default_auto_field = None
-    name = "interfaces"
-
-    def ready(self):
-        import interfaces.signals
